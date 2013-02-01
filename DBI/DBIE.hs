84c0a6ad4b9dd53b893785297288f734b7adce30
Yi Dai <plmday@gmail.com>
2013-02-01 19:47:05 +0100

    Attempted normalization by evaluation

---
+++ DBI/DBIE.hs
@@ -0,0 +1,43 @@
+import Prelude hiding (abs)
+
+
+type Ind = Int
+type Lvl = Int
+
+data Imp
+  = Var Ind
+  | Abs Imp
+  | App Imp Imp
+  | Clo Env Imp
+  deriving (Show)
+
+var :: Ind -> Imp
+var = Var
+
+abs :: Imp -> Imp
+abs = Abs
+
+app :: Imp -> Imp -> Imp
+app = App
+
+type Env = [Imp]
+
+-- an efficient implementation
+
+-- by-name evaluation
+bne :: Env -> Imp -> Imp
+bne env imp = case imp of
+  Var _       -> imp
+  Abs bod     -> Clo env bod
+  App opr opd -> case bne env opr of
+    Clo env bod -> bne (opd : env) bod
+    whn         -> App whn opd
+
+rfy :: Imp -> Imp
+rfy imp = case imp of
+  Clo env bod -> Abs . rfy $ bne (Var 0 : env) bod
+  _           -> imp
+
+nbe :: Imp -> Imp
+nbe imp = rfy (bne [] imp)
+
