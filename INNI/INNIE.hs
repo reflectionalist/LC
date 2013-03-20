2285be2a4f96430e5aa05013083e48b8ec6c2bc4
Yi Dai <plmday@gmail.com>
2013-03-20 12:27:13 +0100

    Renamed `Abs' (`abs') to `All' (`all')

--- INNI/INNIE.hs
+++ INNI/INNIE.hs
@@ -1,10 +1,9 @@
 module INNIE
-  ( var, abs, aps
+  ( var, als, aps
   , norm, form, normalize )
 where
 
 
-import Prelude as P hiding (abs)
 import Data.Map.Strict as M
 import Data.Sequence as S
 
@@ -15,7 +14,7 @@ type Lvl = Int
 
 data Imp
   = Var Nom Ind
-  | Abs Nom Imp
+  | All Nom Imp
   | App Imp Imp
   | Clo Env Nom Imp
   deriving (Show)
@@ -23,8 +22,8 @@ data Imp
 var :: Nom -> Imp
 var nom = Var nom 0
 
-abs :: [Nom] -> Imp -> Imp
-abs = flip (P.foldr Abs)
+als :: [Nom] -> Imp -> Imp
+als = flip (P.foldr All)
 
 aps :: Imp -> [Imp] -> Imp
 aps = P.foldl App
@@ -53,7 +52,7 @@ norm env imp = case imp of
     Exact imp -> imp
     Beyond    -> Var nom (ind - 1)
     None      -> error $ "Unbound variable: " ++ show imp
-  Abs nom bod -> Clo env nom bod
+  All nom bod -> Clo env nom bod
   App opr opd -> case norm env opr of
     Clo sen nom bod -> norm (extend sen nom $ norm env opd) bod
     wnf             -> App wnf (norm env opd)
@@ -63,14 +62,14 @@ shift nam lvl stp imp = case imp of
   Var nom ind | nom /= nam -> imp
               | ind < lvl  -> imp
               | otherwise  -> Var nom (ind + stp)
-  Abs nom bod     -> Abs nom     $ shift nam (if nam == nom then lvl + 1 else lvl) stp bod
+  All nom bod     -> All nom     $ shift nam (if nam == nom then lvl + 1 else lvl) stp bod
   Clo env nom bod -> Clo env nom $ shift nam (if nam == nom then lvl + 1 else lvl) stp bod
   App opr opd     -> App (shift nam lvl stp opr) (shift nam lvl stp opd)
 
 form :: Imp -> Imp
 form imp = case imp of
   Clo env nom bod -> let len = extend (M.map (fmap $ shift nom 0 1) env) nom (var nom)
-                     in  Abs nom $ form (norm len bod)
+                     in  All nom $ form (norm len bod)
   App opr opd     -> App (form opr) (form opd)
   _               -> imp
 
@@ -80,10 +79,10 @@ normalize = form . norm M.empty
 
 tests :: [Imp]
 tests =
-  [ normalize $ abs ["y"] (aps (abs ["x", "y"] (var "x")) [(var "y")])
-  , normalize $ abs ["y"] (aps (abs ["x", "y", "z"] (var "x")) [var "y"])
-  , normalize $ abs ["y"] (aps (abs ["x", "y", "y"] (var "x")) [var "y"])
-  , normalize $ abs ["y"] (aps (abs ["x", "y", "y"] (var "x")) [abs ["x"] (var "y")])
-  , normalize $ abs ["y"] (aps (abs ["x", "y", "y"] (var "x")) [abs ["x", "y"] (var "y")])
-  , normalize $ abs ["y"] (aps (abs ["x", "y", "y"] (var "x")) [abs ["x", "y", "z"] (var "y")]) ]
+  [ normalize $ als ["y"] (aps (als ["x", "y"] (var "x")) [(var "y")])
+  , normalize $ als ["y"] (aps (als ["x", "y", "z"] (var "x")) [var "y"])
+  , normalize $ als ["y"] (aps (als ["x", "y", "y"] (var "x")) [var "y"])
+  , normalize $ als ["y"] (aps (als ["x", "y", "y"] (var "x")) [als ["x"] (var "y")])
+  , normalize $ als ["y"] (aps (als ["x", "y", "y"] (var "x")) [als ["x", "y"] (var "y")])
+  , normalize $ als ["y"] (aps (als ["x", "y", "y"] (var "x")) [als ["x", "y", "z"] (var "y")]) ]
 
