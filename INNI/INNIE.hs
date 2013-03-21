4d14cbf87959da3c4a14d0edadd307d11c542a4c
Yi Dai <plmday@gmail.com>
2013-03-21 14:47:28 +0100

    Refactoring and reformatting

--- INNI/INNIE.hs
+++ INNI/INNIE.hs
@@ -1,6 +1,6 @@
 module INNIE
   ( var, all, app, als, aps
-  , norm, form, normalize )
+  , normalize, deep, norm, nlz )
 where
 
 
@@ -41,6 +41,7 @@ als = flip (P.foldr All)
 aps :: Imp -> [Imp] -> Imp
 aps = P.foldl App
 
+
 type Env = Map Nom (Seq Imp)
 
 data Result a
@@ -50,61 +51,79 @@ data Result a
 
 search :: Env -> Nom -> Ind -> Result Imp
 search env nom ind = case M.lookup nom env of
-  Just seq | ind < S.length seq -> Exact (S.index seq ind)
-           | otherwise          -> Beyond
-  Nothing  -> None
+    Just seq | ind < S.length seq -> Exact (S.index seq ind)
+             | otherwise          -> Beyond
+    Nothing  -> None
 
 extend :: Env -> Nom -> Imp -> Env
 extend env nom imp
   | M.null env = M.singleton nom (S.singleton imp)
   | otherwise  = M.insertWith (S.><) nom (S.singleton imp) env
 
-type Norm r = Identity r
 
-runNorm :: Norm r -> r
-runNorm = runIdentity
+type Normalization r = Identity r
 
-norm :: Env -> Imp -> Norm Imp
-norm env imp = case imp of
-  Var nom ind -> case search env nom ind of
-    Exact imp -> return imp
-    Beyond    -> return $ Var nom (ind - 1)
-    None      -> fail $ "Unbound variable: " ++ show imp
-  All nom bod -> return (Clo env nom bod)
-  App opr opd -> do
-    wnf <- norm env opr
-    case wnf of
-      Clo sen nom bod -> do arg <- norm env opd
-                            norm (extend sen nom arg) bod
-      wnf             -> do arg <- norm env opd
-                            return $ App wnf arg
+run :: Normalization r -> r
+run = runIdentity
+
+normalize :: Env -> Imp -> Normalization Imp
+normalize env imp = case imp of
+    Var nom ind -> case search env nom ind of
+        Exact imp -> return imp
+        Beyond    -> return $ Var nom (ind - 1)
+        None      -> fail $ "Unbound variable: " ++ show imp
+    All nom bod -> return (Clo env nom bod)
+    App opr opd -> do
+        whd <- normalize env opr
+        case whd of
+            Clo sen nom bod -> do arg <- normalize env opd
+                                  normalize (extend sen nom arg) bod
+            _               -> do arg <- normalize env opd
+                                  return $ App whd arg
 
 shift :: Nom -> Lvl -> Int -> Imp -> Imp
 shift nam lvl stp imp = case imp of
-  Var nom ind | nom /= nam -> imp
-              | ind < lvl  -> imp
-              | otherwise  -> Var nom (ind + stp)
-  All nom bod     -> All nom     $ shift nam (if nam == nom then lvl + 1 else lvl) stp bod
-  Clo env nom bod -> Clo env nom $ shift nam (if nam == nom then lvl + 1 else lvl) stp bod
-  App opr opd     -> App (shift nam lvl stp opr) (shift nam lvl stp opd)
+    Var nom ind | nom /= nam -> imp
+                | ind < lvl  -> imp
+                | otherwise  -> Var nom (ind + stp)
+    All nom bod     -> All nom     $ shift nam (level nom) stp bod
+    Clo env nom bod -> Clo env nom $ shift nam (level nom) stp bod
+    App opr opd     -> App (shift nam lvl stp opr) (shift nam lvl stp opd)
+  where
+    level nom = if nom == nam then lvl + 1 else lvl
 
-form :: Imp -> Imp
-form imp = case imp of
-  Clo env nom bod -> let len = extend (M.map (fmap $ shift nom 0 1) env) nom (var nom)
-                     in  All nom $ form $ runNorm (norm len bod)
-  App opr opd     -> App (form opr) (form opd)
-  _               -> imp
+deep :: Imp -> Normalization Imp
+deep imp = case imp of
+    Clo env nom bod -> do let wen = fmap (fmap $ shift nom 0 1) env
+                              len = extend wen nom (var nom)
+                          whd <- normalize len bod
+                          hd  <- deep whd
+                          return (All nom hd)
+    App opr opd     -> do hd  <- deep opr
+                          arg <- deep opd
+                          return (App hd arg)
+    _               -> return imp
 
-normalize :: Imp -> Imp
-normalize = form . runNorm . norm M.empty
+norm :: Env -> Imp -> Normalization Imp
+norm env imp = normalize env imp >>= deep
+
+nlz :: Imp -> Imp
+nlz imp = run (norm M.empty imp)
 
 
 tests :: [Imp]
-tests =
-  [ normalize $ all "y" (app (als ["x", "y"] (var "x")) (var "y"))
-  , normalize $ all "y" (app (als ["x", "y", "z"] (var "x")) (var "y"))
-  , normalize $ all "y" (app (als ["x", "y", "y"] (var "x")) (var "y"))
-  , normalize $ all "y" (app (als ["x", "y", "y"] (var "x")) (all "x" (var "y")))
-  , normalize $ all "y" (app (als ["x", "y", "y"] (var "x")) (als ["x", "y"] (var "y")))
-  , normalize $ all "y" (app (als ["x", "y", "y"] (var "x")) (als ["x", "y", "z"] (var "y"))) ]
+tests = fmap nlz
+  [ all "y" (app (als ["x", "y"] (var "x"))
+            (var "y") )
+  , all "y" (app (als ["x", "y", "z"] (var "x"))
+            (var "y") )
+  , all "y" (app (als ["x", "y", "y"] (var "x"))
+            (var "y") )
+  , all "y" (app (als ["x", "y", "y"] (var "x"))
+            (all "x" (var "y")) )
+  , all "y" (app (als ["x", "y", "y"] (var "x"))
+            (als ["x", "y"] (var "y")) )
+  , all "y" (app (als ["x", "y", "y"] (var "x"))
+            (als ["x", "y", "z"] (var "y")) )
+  ]
 
